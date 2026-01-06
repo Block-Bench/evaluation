bd5e2c1f2ae2ac488ce3e6017767daafa842f187
poamen <pauloams16@gmail.com>
2026-01-06 11:38:39 +0000

    updated

--- dataset_backup_20260105_173203/difficulty_stratified/cleaned/tier1/contracts/ds_t1_012.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.0;
-
-contract SimpleDestruct {
-  function sudicideAnyone() {
-    selfdestruct(msg.sender);
-  }
-
-}
--- dataset_backup_20260105_173203/difficulty_stratified/cleaned/tier1/contracts/ds_t1_046.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.4.10;
-
-contract Caller {
-    function callAddress(address a) {
-        a.call();
-    }
-}
